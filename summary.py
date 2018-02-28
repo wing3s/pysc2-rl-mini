c89922b6ab2bff96f4a1e036f725c5f286c4ea7d
Wen-Hao Lee <wing3s@gmail.com>
2018-02-28 02:20:07 -0800

    add summary write subscriber

---
+++ summary.py
@@ -0,0 +1,20 @@
+import collections
+from tensorboardX import SummaryWriter
+
+
+def writer_fn(args, summary_id, msg_queue):
+    summary_writer = SummaryWriter('{0}/{1}/{2}'.format(args.log_dir, args.map_name, summary_id))
+    with summary_writer:
+        while True:
+            summary = msg_queue.get()
+            print(summary)
+            if summary.action in ['add_scalar', 'add_text', 'add_histogram']:
+                action = getattr(summary_writer, summary.action)
+                action(summary.tag, summary.value1, summary.global_step)
+            elif summary.action == 'add_graph':
+                summary_writer.add_graph(summary.value1, summary.value2)
+
+Summary = collections.namedtuple(
+    'Summary',
+    ['action', 'tag', 'value1', 'value2', 'global_step'])
+Summary.__new__.__defaults__ = (None,) * len(Summary._fields)
