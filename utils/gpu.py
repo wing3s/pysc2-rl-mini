17ed51c8358e8ef4856814ee4ab9f8170f351ee6
Wen-Hao Lee <wing3s@gmail.com>
2018-03-18 06:53:38 -0700

    update cuda func doc

--- utils/gpu.py
+++ utils/gpu.py
@@ -1,10 +1,11 @@
 def cuda(ts, gpu_id, **kwargs):
-    """Apply tensor.cuda() if gpu_id is valid
-        Args:
-            ts (torch.tensor)
-            gpu_id (int) - The destination GPU id. -1 for CPU only.
-        Returns:
-            ts (torch.tensor)
+    """Apply tensor.cuda() if gpu_id is valid.
+
+    Args:
+        ts (torch.tensor)
+        gpu_id (int) - The destination GPU id. -1 for CPU only.
+    Returns:
+        ts (torch.tensor)
     """
     assert gpu_id >= -1
     if gpu_id >= 0:
