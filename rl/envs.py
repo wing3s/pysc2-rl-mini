1661c4d873e37db90a9590bae9f102250314e9d4
Wen-Hao Lee <wing3s@gmail.com>
2018-03-26 23:43:47 -0700

    change config import path

--- rl/envs.py
+++ rl/envs.py
@@ -1,12 +1,15 @@
 import yaml
 import numpy as np
+from os import path
 
 from pysc2.env import available_actions_printer
 from pysc2.env import sc2_env
 from pysc2.lib import features
 from pysc2.lib import actions
 
-with open("sc2_config.yml", 'r') as ymlfile:
+
+sc2_f_path = path.abspath(path.join(path.dirname(__file__), "..", "configs", "sc2_config.yml"))
+with open(sc2_f_path, 'r') as ymlfile:
     sc2_cfg = yaml.load(ymlfile)
 
 
