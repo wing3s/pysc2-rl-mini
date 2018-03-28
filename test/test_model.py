7806944db053f12b1faf2f40149934e92f3f6287
Wen-Hao Lee <wing3s@gmail.com>
2018-03-28 00:38:04 -0700

    add test init weights

--- test/test_model.py
+++ test/test_model.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 
 from torch.autograd import Variable
@@ -12,11 +13,15 @@ class ModelTest(unittest.TestCase):
     def setUp(self):
         torch.manual_seed = 123
         self.dtype = np.float32
+        self.minimap_channels = 2
+        self.screen_channels = 2
+        self.screen_resolution = 4
         self.num_action = 7
+
         self.fully_conv = model.FullyConv(
-            minimap_channels=2,
-            screen_channels=2,
-            screen_resolution=4,
+            self.minimap_channels,
+            self.screen_channels,
+            self.screen_resolution,
             num_action=self.num_action)
 
     def testMaskUnavailableActions(self):
@@ -33,3 +38,12 @@ class ModelTest(unittest.TestCase):
         self.assertAlmostEqual(policy_vb.sum().data[0], 1.)
         self.assertAlmostEqual(available_action_vb.sum().data[0], 3.)
         self.assertAlmostEqual(masked_policy_vb.sum().data[0], 1.)
+
+    def testInitWeights(self):
+        conv1 = nn.Conv2d(2, 2, 3, stride=2, padding=1)
+        before_weight = conv1.weight.clone()
+        model.init_weights(conv1)
+
+        self.assertTrue(type(conv1.weight) is torch.nn.parameter.Parameter)
+        self.assertTrue(type(conv1.bias) is torch.nn.parameter.Parameter)
+        self.assertFalse(torch.equal(before_weight, conv1.weight))
