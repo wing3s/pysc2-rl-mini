096c6567fe6282843b48b5d83d6b5bd4c9db8358
Wen-Hao Lee <wing3s@gmail.com>
2018-05-05 23:52:14 -0700

    added subprocvecEnv class init

--- rl/utils/subproc_vec_env.py
+++ rl/utils/subproc_vec_env.py
@@ -3,22 +3,43 @@ import numpy as np
 from torch.multiprocessing import Process, Pipe
 
 
-def worker(conn_worker, conn_parent, env_fn_wrapper):
+def worker(worker_conn, mgr_conn, env_fn_wrapper):
     """ Remote worker to receive command and send back state"""
-    conn_parent.close()
+    mgr_conn.close()
     env = env_func_wrapper.x()
     while True:
-        cmd, data = conn_worker.recv()
+        cmd, data = worker_conn.recv()
         if cmd == 'step':
             ob, reward, done, info = env.step(data)
             if done:
                 ob = env.reset()
-            conn_worker.send((ob, reward, done, info))
+            worker_conn.send((ob, reward, done, info))
         elif cmd == 'reset':
             ob = env.reset()
-            conn_worker.send(ob)
+            worker_conn.send(ob)
         elif cmd == 'close':
-            conn_worker.close()
+            worker_conn.close()
             break
         else:
             raise NotImplementedError
+
+
+class SubprocVecEnv(VecEnv):
+    def __init__(self, env_fns, spaces=None):
+        self.waiting = False
+        self.closed = False
+        nenvs = len(env_fns)
+        self.mgr_conns, self.worker_conns = zip(*[Pipe() for _ in range(nenvs)])
+        self.ps = [
+            Process(
+                target=worker,
+                args=(worker_conn, mgr_conn, CloudpickleWrapper(env_fn)))
+            for (worker_conn, mgr_conn, env_fn) in zip(self.worker_conns, self.mgr_conns, env_fns)
+        ]
+        for p in self.ps:
+            p.daemon = True
+            p.start
+        for worker_conn in self.worker_conns:
+            worker_conn.close()
+
+        super(SubprocVecEnv, self).__init__()
